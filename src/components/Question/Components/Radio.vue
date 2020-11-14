8085130e4f4d22ef449aada8be57fa00f62066db
99611400 <657793418@qq.com>
2020-11-14 09:26:23 +0800

    完成了基本的一些工作 剩下的是优化配置

---
+++ src/components/Question/Components/Radio.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-radio-group v-model="radio">
+    <el-radio v-for="(answerhh,index) in answer" v-bind:key="answerhh" :label=index  size="mini" >
+      <h8>{{answerhh}}</h8>
+    </el-radio>
+  </el-radio-group>
+</template>
+
+<script>
+export default {
+  props: ['answer'],
+  data () {
+    return {
+      radio: -1
+    };
+  }
+}
+</script>
---
+++ src/components/Question/Qustion.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="divFather" >
+    <div  >{{message}} </div>
+    <Radio v-bind:answer="answer"></Radio>
+
+  </div>
+</template>
+
+<script>
+import Radio from "@/components/Question/Components/Radio";
+
+export default {
+  name: "question",
+  props:['answer'],
+  data:function() {
+    return {
+    message:""
+  };
+},
+  components:{
+    Radio
+  }
+}
+</script>
+
+<style scoped>
+  #divFather{
+    display: flex;
+    flex-direction:  row;
+    margin-left: 42px;
+  }
+</style>
